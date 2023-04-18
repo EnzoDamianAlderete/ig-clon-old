3828ff33cabae4e869e8cf77a2ab02b282bf8d1e
EnzoDamianAlderete <enzoalderete04@gmail.com>
2023-04-17 22:48:43 -0300

    se agrega 404

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,7 @@ import React from 'react';
 import {BrowserRouter, Routes, Route} from "react-router-dom";
 import Perfil from './containers/perfil';
 import { ContextProvider } from './context/AppContext';
+import Page404 from './containers/Page404';
 
 function App() {
   return (
@@ -18,6 +19,7 @@ function App() {
       <Route path='/' element={<HomeContainer/>} />
       <Route path='/chats' element={<ChatsPage/>} />
       <Route path='/perfil' element={<Perfil/>} />
+      <Route path='*' element={<Page404/>} />
     </Routes>
     </ContextProvider>
     </BrowserRouter>
